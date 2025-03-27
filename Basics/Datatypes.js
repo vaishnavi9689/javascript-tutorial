e69552fad67b856577d056874aaad59f87041453
vaishnavi9689 <159602321+vaishnavi9689@users.noreply.github.com>
2025-03-27 06:35:05 +0000

    Changed done

---
+++ Basics/Datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log(3+3);
+
+let name="Vaishnavi"
+let age="18";
+let isloggedIn=true
+let ageInNumber=Number(age)
+
+console.log(typeof(age))
+console.log(typeof(isloggedIn))
+console.log(typeof(ageInNumber))
---
+++ Basics/Variables.js
@@ -0,0 +1,13 @@
+const acoountId=144325
+let accountEmail="vaishnavishinde@gmail.com"
+var accountPassword="1234"
+accountCity="Jaipur"
+
+accountId=2
+
+accountEmail="tanishqdoke@gmail.com"
+accountPassword="5678"
+accountCity="Delhi"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
